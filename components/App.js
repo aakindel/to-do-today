efeb368f3464425f066992de73dfbc28dd1d27cf
Ayo Akindele <a.akindele01@gmail.com>
2021-07-13 16:21:44 -0600

    update local version of Paragraph text onInput and disable SSR to avoid clash between server and client uuids

--- components/App.js
+++ components/App.js
@@ -1,11 +1,12 @@
 import { v4 as uuidv4 } from 'uuid';
 import styles from '../styles/App.module.css'
 
-function Paragraph({id, text}) {
+function Paragraph({id, text, updateLineInLines}) {
   return (
     <div id={id} className={styles.pg_block} suppressHydrationWarning
       contentEditable="true" suppressContentEditableWarning={true} 
       placeholder="Add To-Do" 
+      onInput={() => {updateLineInLines()}}
     >
       {text}
     </div>
@@ -17,9 +18,21 @@ export default function App() {
   const lines = [{id: uuidv4(), text: ""}];
   console.log(lines);
 
+  const updateLineInLines = () => {
+    // get currentLineText & currentLineIndex
+    const currentLine = document.activeElement;
+    const currentLineText = currentLine.innerText;
+    const currentLineId = currentLine.attributes.getNamedItem("id").value;
+    const currentLineIndex = lines.findIndex(x => x.id === currentLineId);
+
+    // update current line text in Lines array with currentLineText from DOM
+    lines[currentLineIndex].text = currentLineText;
+    console.log('lines: '+ JSON.stringify(lines));
+  }
+
   return (
     lines.map((line) => {
-      return <Paragraph key={line.id} id={line.id} text={line.text} />
+      return <div key={line.id}><Paragraph id={line.id} text={line.text} updateLineInLines={updateLineInLines} /></div>
     })
   );
 }
